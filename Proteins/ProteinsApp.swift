1a21f725ffcb31d3e5bbb02112cd78ebbd535422
andru196 <andru196@icloud.com>
2022-01-09 20:20:37 +0300

    Initial Commit

---
+++ Proteins/ProteinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinsApp.swift
+//  Proteins
+//
+//  Created by Andrew Tarasow on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProteinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
