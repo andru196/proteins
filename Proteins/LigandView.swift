45fc0c2fb174f6b597276c1517592980e9d9b256
andru196 <andru196@icloud.com>
2022-02-18 15:24:52 +0300

    share

--- Proteins/LigandView.swift
+++ Proteins/LigandView.swift
@@ -115,7 +115,8 @@ struct LigandView: View {
             else {
                 HStack {
                     Button(action: {
-                        
+                        share(items: [_scnView.scenekitClass.view.snapshot(), ligand.name],
+                              excludedActivityTypes: [.postToFlickr])
                     }) {
                         Image(systemName: "square.and.arrow.up")
                             .padding(30)
@@ -272,6 +273,23 @@ struct LigandView: View {
         return scene
     }
     
+    @discardableResult
+    func share(
+        items: [Any],
+        excludedActivityTypes: [UIActivity.ActivityType]? = nil
+    ) -> Bool {
+        guard let source = UIApplication.shared.windows.last?.rootViewController else {
+            return false
+        }
+        let vc = UIActivityViewController(
+            activityItems: items,
+            applicationActivities: nil
+        )
+        vc.excludedActivityTypes = excludedActivityTypes
+        vc.popoverPresentationController?.sourceView = source.view
+        source.present(vc, animated: true)
+        return true
+    }
 }
 
 
